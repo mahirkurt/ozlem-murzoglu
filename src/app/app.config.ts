9d81a4bd63a5bd5dc98470a7d3f4a55ec9caaf72
Mahir Kurt <mahirkurt@aof.anadolu.edu.tr>
2025-08-24 23:43:17 +0300

    fix: Translation files loading issue - moved to public/assets/i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http);
 }
 
 export const appConfig: ApplicationConfig = {
