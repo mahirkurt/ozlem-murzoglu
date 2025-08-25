3a66967e68360fb326f389888210a5f0aaed6350
Mahir Kurt <mahirkurt@aof.anadolu.edu.tr>
2025-08-25 07:21:21 +0300

    feat: Vercel'den Firebase Hosting'e geçiş tamamlandı
    - Firebase CLI kurulumu ve proje yapılandırması
    - firebase.json ve .firebaserc dosyaları oluşturuldu
    - Environment dosyalarına Firebase config eklendi
    - Vercel.json kaldırıldı
    - Firebase ve AngularFire paketleri yüklendi
    - Proje başarıyla Firebase'e deploy edildi
    
    Hosting URLs:
    - https://dr-murzoglu.web.app
    - https://dr-murzoglu.firebaseapp.com

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBm7xVx7n-kmo1i-xqxvPzv0epzXPsBPk0",
+    authDomain: "dr-murzoglu.firebaseapp.com",
+    projectId: "dr-murzoglu",
+    storageBucket: "dr-murzoglu.firebasestorage.app",
+    messagingSenderId: "163292402794",
+    appId: "1:163292402794:web:43eb6681651c8131e96613",
+    measurementId: "G-2JE4QNXFD3"
+  }
+};
