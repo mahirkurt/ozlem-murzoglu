b06c963d9bad7e531b549c7d3a08fa9b287196a5
Mahir Kurt <mahirkurt@aof.anadolu.edu.tr>
2025-12-09 06:58:03 +0300

    fix: comprehensive i18n repair and structure fixes [deploy-success]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive'
 import { DoctorBioComponent } from '../../components/doctor-bio/doctor-bio';
 import { ContactCtaComponent } from '../../components/contact-cta/contact-cta.component';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -15,7 +16,8 @@ import { ContactCtaComponent } from '../../components/contact-cta/contact-cta.co
     ServicesSectionComponent,
     ScrollRevealDirective,
     DoctorBioComponent,
-    ContactCtaComponent
+    ContactCtaComponent,
+
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
